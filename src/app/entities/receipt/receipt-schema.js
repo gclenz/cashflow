ea6e8e098bfe079da097357e9efe201cdc5b6adb
Gabriel Lenz <gabrielclenz@gmail.com>
2020-05-25 19:13:41 -0300

    fix: wrong type and value validation

--- src/app/entities/receipt/receipt-schema.js
+++ src/app/entities/receipt/receipt-schema.js
@@ -12,16 +12,6 @@ module.exports = async (receipt) => {
       value: Yup.number().required(),
     });
 
-    const isPositiveValue = Math.sign(Number(receipt.value));
-
-    if (receipt.type === 'in' && isPositiveValue === -1) {
-      throw new Error({ error: 'An in receipt must have a positive value' });
-    }
-
-    if (receipt.type === 'out' && isPositiveValue === 1) {
-      throw new Error({ error: 'An out receipt must have a negative value' });
-    }
-
     return await schema.validate(receipt, { abortEarly: false });
   } catch (err) {
     throw new Error({ error: 'Validation failed.', err });
--- src/app/entities/receipt/receipt.js
+++ src/app/entities/receipt/receipt.js
@@ -2,6 +2,20 @@ module.exports = function buildMakeReceipt(schema) {
   return ({ type, category_id, date, description, value, user_id } = {}) => {
     schema({ type, category_id, date, description, value });
 
+    function checkIfValueIsCorrect() {
+      const valueSign = Math.sign(Number(value));
+
+      if (type === 'in' && valueSign === -1) {
+        return false;
+      }
+
+      if (type === 'out' && valueSign === 1) {
+        return false;
+      }
+
+      return true;
+    }
+
     function checkIfUserExists(id) {
       return true;
     }
@@ -12,9 +26,14 @@ module.exports = function buildMakeReceipt(schema) {
 
     const userExists = checkIfUserExists(user_id);
     const categoryExists = checkIfCategoryExists(user_id);
+    const valueHasRightSign = checkIfValueIsCorrect();
 
     if (!userExists) throw new Error('User not found.');
     if (!categoryExists) throw new Error('Category not found.');
+    if (!valueHasRightSign)
+      throw new Error(
+        'An in receipt must have a positive value and an out receipt must have a negative value.'
+      );
 
     return Object.freeze({
       getType: () => type,
